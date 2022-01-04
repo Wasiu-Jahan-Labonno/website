09dc6a591bcc5a02823955f7f3cf7169ef1c06a9
Wasiu-Jahan-Labonno <wasiujahan764@gmail.com>
2022-01-04 18:49:41 +0600

    tab prb

--- js/myjs.js
+++ js/myjs.js
@@ -66,3 +66,11 @@ btn.addEventListener("click" , (event)=>{
 
 
 
+document.querySelector('.button1').onmousemove = function (e) {
+
+var x = e.pageX - e.target.offsetLeft;
+var y = e.pageY - e.target.offsetTop;
+
+e.target.style.setProperty('--x', x + 'px');
+e.target.style.setProperty('--y', y + 'px');
+};
